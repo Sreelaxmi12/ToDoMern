848d635c9105578d05d15e97a6f6ec132adc2627
Sreelaxmi12 <sreelaxmi0803@gmail.com>
2023-06-03 22:35:50 +0530

    Update plss

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,15 @@
+
+
+
+const Header = () => {
+    return (
+      <header>
+        <h1>My ToDo App</h1>
+        <div className="intro">
+          <p>Add, Edit, Delete your todos</p>
+        </div>
+      </header>
+    );
+}
+
+export default Header;
