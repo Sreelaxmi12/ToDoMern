94c9ee84ad117e1dff363e72fc2072ac6a40cb15
Sreelaxmi-Squbix <sreelaxmi.sukhari@squbix.com>
2023-11-02 15:25:42 +0530

    Updated CSS and backend fixed

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,15 +1,12 @@
-
-
-
 const Header = () => {
-    return (
-      <header>
-        <h1>My ToDo App</h1>
-        <div className="intro">
-          <p>Add, Edit, Delete your todos</p>
-        </div>
-      </header>
-    );
-}
+  return (
+    <header>
+      <h1>TODO APP</h1>
+      <div className="intro">
+        <p>A simple app to help you maintain your todos.</p>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
--- server/database/db.js
+++ server/database/db.js
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -7,23 +7,21 @@ const USERNAME = process.env.DB_USERNAME;
 const PASSWORD = process.env.DB_PASSWORD;
 
 const Connection = () => {
+  const MONGODB_URL = `mongodb://${USERNAME}:${PASSWORD}@ac-it3pqeu-shard-00-00.kvwkjb9.mongodb.net:27017,ac-it3pqeu-shard-00-01.kvwkjb9.mongodb.net:27017,ac-it3pqeu-shard-00-02.kvwkjb9.mongodb.net:27017/?ssl=true&replicaSet=atlas-fj5omq-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  // mongoose.connect(MONGODB_URL, {useNewUrlParser: true});
+  mongoose.connect(MONGODB_URL, { useNewUrlParser: true });
 
-    const MONGODB_URL =
-      `mongodb://${USERNAME}:${PASSWORD}@ac-it3pqeu-shard-00-00.kvwkjb9.mongodb.net:27017,ac-it3pqeu-shard-00-01.kvwkjb9.mongodb.net:27017,ac-it3pqeu-shard-00-02.kvwkjb9.mongodb.net:27017/?ssl=true&replicaSet=atlas-fj5omq-shard-0&authSource=admin&retryWrites=true&w=majority`;
-    // mongoose.connect(MONGODB_URL, {useNewUrlParser: true});
-    mongoose.connect(MONGODB_URL, {useNewUrlParser: true});
+  mongoose.connection.on("connected", () => {
+    console.log("Database connected successfully!");
+  });
 
-    mongoose.connection.on('connected', () => {
-        console.log('Database connected successfully!');    
-    })
+  mongoose.connection.on("disconnected", () => {
+    console.log("Database disconnected");
+  });
 
-    mongoose.connection.on("disconnected", () => {
-      console.log("Database disconnected");
-    });
+  mongoose.connection.on("error", () => {
+    console.log("Error while connecting with the database", error.message);
+  });
+};
 
-    mongoose.connection.on('error', () => {
-        console.log('Error while connecting with the database', error.message);
-    })
-}
-
-export default Connection
+export default Connection;
